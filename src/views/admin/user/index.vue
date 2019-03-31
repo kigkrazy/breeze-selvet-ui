7ef66fbd54b6e75be8b3c6c6155098598b9a3d38
冷冷 <wangiegie@gmail.com>
2019-03-31 13:03:18 +0800

    :bug: Fixing a bug.
    #189 用户编辑界面加载异常
    #192 pigx-ui显示组织机构树和请求提示问题

--- src/views/admin/user/index.vue
+++ src/views/admin/user/index.vue
@@ -115,7 +115,7 @@
 <script>
   import {addObj, delObj, fetchList, getObj, putObj} from "@/api/admin/user";
   import {deptRoleList} from "@/api/admin/role";
-  import {fetchDeptTree,fetchTree} from "@/api/admin/dept";
+  import {fetchTree} from "@/api/admin/dept";
   import {tableOption} from '@/const/crud/admin/user';
   import {mapGetters} from "vuex";
   import {constants} from 'fs';
@@ -175,7 +175,7 @@
     },
     methods: {
       init() {
-        fetchDeptTree().then(response => {
+        fetchTree().then(response => {
           this.treeData = response.data.data;
         });
       },
